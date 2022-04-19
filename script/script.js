cbc60ba5b5d7b80f567720aa73de3cfab918ae06
MIDOKWEB <103947203+MIDOKWEB@users.noreply.github.com>
2022-04-19 19:58:58 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
     if (scrollPosition) window.scrollTo(0, scrollPosition)
 })
 
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 window.onbeforeunload = function() {
     localStorage.setItem('scrollPosition', window.scrollY)
 }
 
-
+const dropdown_element = document.getElementById('dropdown')
 let dropdown_flag = true
 
 function resizeEvent() {
@@ -17,16 +19,51 @@ function resizeEvent() {
 
     if (width >= 650) {
         dropdown_flag = true
-        document.getElementById('dropdown').style.display = 'none'
+        dropdown_element.style.display = 'none'
     }
 }
 
 function dropdown() {
     if (dropdown_flag) {
-        document.getElementById('dropdown').style.display = 'flex'
+        dropdown_element.style.display = 'flex'
         dropdown_flag = !dropdown_flag
     } else {
-        document.getElementById('dropdown').style.display = 'none'
+        dropdown_element.style.display = 'none'
         dropdown_flag = !dropdown_flag
     }
+}
+
+window.addEventListener('scroll', scrollEvent)
+
+const topbtn = document.getElementById('topBtn')
+
+let fade_flag = 0
+async function scrollEvent() {
+    let current_scroll_position = window.scrollY
+    const anchor = document.getElementById('section2')
+    let anchor_element = anchor.getBoundingClientRect()
+
+    if (anchor_element.top < 5) {
+        topbtn.style.display = 'flex'
+        if (fade_flag == 0) {
+            for (let i = 0; i < 50; i++) {
+                await delay(5)
+                topbtn.style.opacity = i * 0.02
+            }
+        }
+        fade_flag = 1
+    } else {
+        if (fade_flag == 1) {
+            for (let j = 50; j > 0; j--) {
+                await delay(5)
+                topbtn.style.opacity = j * 0.02
+            }
+        }
+        topbtn.style.display = 'none'
+        fade_flag = 0
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo(0, 0)
 }
