57bbde617b7e1d50d311e13f2ddd76a7848b8ac9
MIDOKWEB <103947203+MIDOKWEB@users.noreply.github.com>
2022-07-05 00:03:05 +0200

    Nerf error message

--- vue/gallery.js
+++ vue/gallery.js
@@ -21,15 +21,6 @@ const app = Vue.createApp({
         onScroll() {
             this.autoScroll()
         },
-        isInViewport(element) {
-            const rect = element.getBoundingClientRect();
-            return (
-                rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-            );
-        },
         onResize() {
             this.autoScroll()
         },
